6d99e42810ba3fce6c37271f05eb96d072b99827
BinMa <binma@thoughtworks.com>
2017-09-26 17:31:26 +0800

    Add task producer

---
+++ task-intake/src/main/java/com/thoughtworks/DasTaskIntakeApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasTaskIntakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DasTaskIntakeApplication.class, args);
+    }
+}
---
+++ task-intake/src/main/java/com/thoughtworks/TaskProcessor.java
@@ -0,0 +1,32 @@
+package com.thoughtworks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.task.launcher.TaskLaunchRequest;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@EnableBinding(Source.class)
+public class TaskProcessor {
+
+    @Autowired
+    private Source source;
+
+    public void publishRequest(String payload) {
+        String url = "maven://com.thoughtworks:task:jar:0.0.1-SNAPSHOT";
+        List<String> input = new ArrayList<>(Arrays.asList(payload.split(",")));
+
+        TaskLaunchRequest request = new TaskLaunchRequest(url, input, null, null, null);
+
+        System.out.println("Created task request");
+
+        GenericMessage<TaskLaunchRequest> message = new GenericMessage<>(request);
+        this.source.output().send(message);
+    }
+}
